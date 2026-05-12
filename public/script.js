c7c80c178a597f3e78a206a4e28a26050154c2a0
adwaitdubey14 <adwaitdubey14@gmail.com>
2026-05-12 12:24:42 +0530

    manifest fic

--- public/script.js
+++ public/script.js
@@ -359,3 +359,15 @@ function setupAutoCaps() {
     });
 
 }
+
+if ('serviceWorker' in navigator) {
+
+    window.addEventListener('load', () => {
+
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(() => console.log('✅ PWA Ready'))
+            .catch(err => console.log('SW Error:', err));
+
+    });
+
+}
